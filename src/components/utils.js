91c121527ef0e00545e5174ab09dbbf44be3e1b8
RENCHIE EUGENIOUS YANG <ryan0042@student.monash.edu>
2022-02-14 00:16:30 +1100

    final

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,22 +1,3 @@
-//const FOREX_API_KEY = "f51bf1158ca70b9395db3f4616742559";
-
-//const fetchConvertionRates = async (from, to, amount) => {}
-
-
-
-const fetchCryptoPrice = async (product_id) => {
-    let result;
-    const url = `https://api.exchange.coinbase.com/products/${product_id}/ticker`;
-    const options = {method: 'GET', headers: {Accept: 'application/json'}};
-
-    await fetch(url, options)
-    .then(res => res.json())
-    .then(json => result = json.price)
-    .catch(err => console.error('error:' + err));
-
-    return result;
-}
-
 export const convertDataToAud = (data) => {
     //const convertionRate = fetchConvertionRates('USD', 'AUD', 1)
     for (let i = 0; i < data.length; i++) {
